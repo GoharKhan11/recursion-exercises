23812b8bd3e26347f83898cdcf325787aa7f0891
Gohar <goharayub2008@gmail.com>
2022-09-09 17:06:04 -0400

    Complete basic TreeNode class

--- binary-search-tree/lib/tree_node.rb
+++ binary-search-tree/lib/tree_node.rb
@@ -1,9 +1,23 @@
 class TreeNode
 
+    include Comparable
+
     def initialize(data=0, left_child=nil, right_child=nil)
+        # any, TreeNode, TreeNode -> TreeNode
         @data = data
         @left_child = left_child
         @right_child = right_child
+    end
+
+    attr_accessor :data
+    attr_accessor :left_child
+    attr_accessor :right_child
+
+    def <=>(other_tree_node)
+        # TreeNode -> bool
+        # Compares the data in the TreeNodes using
+        # the comparable module
+        @data <=> other_tree_node.data
     end
 
 end
